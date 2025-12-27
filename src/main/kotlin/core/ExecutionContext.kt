4bf7a8ca413b84c8e191cb91d2dbf0d71fc0a1d7
Klimm-Max <klimm.max@gmail.com>
2025-12-27 11:52:49 +0100

    using a threadsafe state impl

--- src/main/kotlin/core/ExecutionContext.kt
+++ src/main/kotlin/core/ExecutionContext.kt
@@ -2,10 +2,12 @@ package io.github.klimmmax.core
 
 import kotlin.random.Random
 import java.time.Clock
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 
 
 class ExecutionContext internal constructor(
     val random: Random = Random.Default,
     val clock: Clock = Clock.systemUTC(),
-    val state: MutableMap<String, Any> = mutableMapOf()
+    val state: ConcurrentMap<String, Any> = ConcurrentHashMap()
 )
